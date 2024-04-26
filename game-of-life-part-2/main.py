c535b1423ea46aa5437a3c75b75fb8eb4a94647e
Elias Bauer <91609312+EliasBauer@users.noreply.github.com>
2024-04-26 07:18:35 +0000

    last commit

--- game-of-life-part-2/main.py
+++ game-of-life-part-2/main.py
@@ -1,61 +1,65 @@
 
 
                                            
-def findeAlleNachbarn(zellen, reihen_index ,zellen_index):
-   #nachbarn links
-      lebende_nachbarn = 0
-      if zellen_index -1 >= 0 :
-         n_links = zellen[reihen_index][zellen_index -1]
-         if n_links=='x':
-         #  print("links gefunden")
-          lebende_nachbarn += 1 
-   #print('links','lebende_nachbarn', lebende_nachbarn) 
+def findAllNeighbors(cells, row_index ,cells_index):
+   #neighbor left
+      living_neighbors = 0
+      if cells_index -1 >= 0 :
+         n_left = cells[row_index][cells_index -1]
+         if n_left=='x':
+          living_neighbors += 1  
+   #prüft, ob links ein nachbar ist
         
-   #nachbarn rechts 
-      if zellen_index + 1 < len(zellen[reihen_index]):
-         n_rechts = zellen[reihen_index][zellen_index +1]
-         if n_rechts =='x':
-            lebende_nachbarn += 1
+   #neighbor right 
+      if cells_index + 1 < len(cells[row_index]):
+         n_right = cells[row_index][cells_index +1]
+         if n_right =='x':
+            living_neighbors += 1
+   #prüft, ob rechts ein nachbar ist
   
-   #nachbarn oben 
-      if reihen_index -1 >= 0 :
-         n_oben = zellen[reihen_index-1 ][zellen_index]
-         if n_oben=='x':
-         # print("oben gefunden")
-            lebende_nachbarn += 1
-  
+   #neighbor up 
+      if row_index -1 >= 0 :
+         n_up = cells[row_index-1 ][cells_index]
+         if n_up=='x':
+            living_neighbors += 1
+   #prüft, ob oberhalb ein nachbar ist
    
-   #nachbarn unten 
-      if reihen_index + 1 < len(zellen[zellen_index]): 
-         n_unten = zellen[reihen_index +1 ][zellen_index]
-         if n_unten == 'x':
-           lebende_nachbarn += 1
+   #neighbor down 
+      if row_index + 1 < len(cells[cells_index]): 
+         n_down = cells[row_index +1 ][cells_index]
+         if n_down == 'x':
+           living_neighbors += 1
+   #prüft, ob links ein nachbar ist
   
-   #diagonal unten rechts
-      if reihen_index +1 < len(zellen[zellen_index]) and zellen_index +1 < len(zellen[reihen_index]):
-         n_unten_rechts = zellen[reihen_index +1][zellen_index +1]
-         if n_unten_rechts == 'x':
-            lebende_nachbarn += 1
+   #diagonal lower right
+      if row_index +1 < len(cells[cells_index]) and cells_index +1 < len(cells[row_index]):
+         n_lower_right = cells[row_index +1][cells_index +1]
+         if n_lower_right == 'x':
+            living_neighbors += 1
+   #prüft, ob unten rechts ein nachbar ist
   
-   #diagonal unten links!
-      if reihen_index +1 < len(zellen[zellen_index]) and zellen_index -1 >= 0:
-         n_unten_links = zellen[reihen_index +1][zellen_index -1]
-         if n_unten_links == 'x':
-            lebende_nachbarn += 1
+   #diagonal lower left!
+      if row_index +1 < len(cells[cells_index]) and cells_index -1 >= 0:
+         n_lower_left = cells[row_index +1][cells_index -1]
+         if n_lower_left == 'x':
+            living_neighbors += 1
+   #prüft, ob unten links ein nachbar ist
    
    
-   #diagonal oben links   
-      if reihen_index -1 >= 0 and zellen_index -1 >= 0:
-         n_oben_links = zellen[reihen_index -1][zellen_index -1]
-         if n_oben_links == 'x':
-            lebende_nachbarn += 1
+   #diagonal up left   
+      if row_index -1 >= 0 and cells_index -1 >= 0:
+         n_upper_left = cells[row_index -1][cells_index -1]
+         if n_upper_left == 'x':
+            living_neighbors += 1
+   #prüft, ob oben links ein nachbar ist
   
-   #diagonal oben rechts   
-      if reihen_index  -1 >= 0 and zellen_index + 1 < len(zellen[reihen_index]):
-         n_oben_rechts = zellen[reihen_index -1][zellen_index +1] 
-         if n_oben_rechts == 'x':
-            lebende_nachbarn += 1
-      return lebende_nachbarn
+   #diagonal up right   
+      if row_index  -1 >= 0 and cells_index + 1 < len(cells[row_index]):
+         n_upper_right = cells[row_index -1][cells_index +1] 
+         if n_upper_right == 'x':
+            living_neighbors += 1
+      return living_neighbors
+   #prüft, ob oben rechts ein nachbar ist
    
 def createEmptyCells(original_cells):
    result = []
@@ -63,38 +67,37 @@ def createEmptyCells(original_cells):
       row_list = []
       for i in range(len(row)):
          row_list.append('')
-      result.append(row_list)
+      result.append(row_list)#fügt zur original eingetippten liste der elemente hinzu
    return result
       
 
-def gameOfLife(original_cells):
-   leere_zellen = createEmptyCells(original_cells)
-   neue_zellen = lifeAndDeath(leere_zellen, original_cells) 
-   for reihe in neue_zellen:
-       print(reihe)
-   return neue_zellen       
+def gameOfLife(original_cells):#leere zellen für die neu-Konfiguration der zellen
+   empty_cells = createEmptyCells(original_cells)#obere funktion wird aufgerufen
+   new_cells = lifeAndDeath(empty_cells, original_cells) 
+   for row in new_cells:
+       print(row)
+   return new_cells   #neue zellen für neue liste erstellt    
          
-def lifeAndDeath(neue_zellen, original_cells):         
-   for reihen_index, reihe in enumerate(original_cells):
-      for zellen_index, zelle in enumerate(reihe):
-      # print('\n\nreihe:',reihen_index, 'index:',zellen_index)
-         if zelle != 'x': # wenn wir tot sind
-            nachbarn = findeAlleNachbarn(original_cells, reihen_index ,zellen_index)
-            if nachbarn == 3:
-               neue_zellen[reihen_index][zellen_index] = 'x'
+def lifeAndDeath(new_cells, original_cells):         
+   for row_index, row in enumerate(original_cells):#zählt die Positionen hoch
+      for cells_index, cell in enumerate(row):#zählt die reihen hoch
+         if cell != 'x': #wenn die zelle tot ist
+            neighbor = findAllNeighbors(original_cells, row_index ,cells_index)
+            if neighbor == 3: #bei genau 3 nachbarn wird eine zelle 'geboren' ansonsten bleibt sie tot
+               new_cells[row_index][cells_index] = 'x'
             else:
-               neue_zellen[reihen_index][zellen_index] = ' '   
-         else: # wenn wir leben
-            nachbarn = findeAlleNachbarn(original_cells, reihen_index ,zellen_index)
-            if nachbarn < 2:
-               neue_zellen[reihen_index][zellen_index] = ' '
-            if nachbarn == 2 or nachbarn == 3 :
-               neue_zellen[reihen_index][zellen_index] = 'x'   
-            if nachbarn > 3:
-               neue_zellen[reihen_index][zellen_index] = ' '
-   return neue_zellen
+               new_cells[row_index][cells_index] = ' '   
+         else: # wenn die zelle lebt
+            neighbor = findAllNeighbors(original_cells, row_index ,cells_index)
+            if neighbor < 2: #wenn eine zelle weniger als zwei nachbarn hat stirbt sie
+               new_cells[row_index][cells_index] = ' '
+            if neighbor == 2 or neighbor == 3 : #wenn eine zelle genau 2 oder 3 Nachbarn hat lebt sie weiter
+               new_cells[row_index][cells_index] = 'x'   
+            if neighbor > 3: #wenn eine zelle mehr als 3 nachbarn hat stirbt sie
+               new_cells[row_index][cells_index] = ' '
+   return new_cells
       
-def formatCells(cell_string):
+def formatCells(cell_string):#unterscheidet die eingegebenen Charaktere um eine Liste zu erstellen
    result = [[]]
    row = 0
    for char in cell_string:
@@ -104,28 +107,31 @@ def formatCells(cell_string):
          result[row].append('x') 
       if char == ',':
          row = row + 1
-         result.append([])
+         result.append([]) #neue liste (unten) hinzugefügt
    return result
            
      
 #wenn (leer) dann in liste ' '
-#wenn , dann reihe+1
-#wenn x dann x in zelle eintragen
+#wenn , dann row+1
+#wenn x dann x in cell eintragen
 def main():
    print("game of life")
    
    # für anzahl generationen
-   number_generations = int(input('Anzahl der Generationen:'))
-   cell_string = input('5 elemente pro reihe (leerzeichen('') oder x):')
+   number_generations = int(input('Number of Generations:'))
+   #eingabe der Liste x/'
+   cell_string = input('elements per row (space or x):')
    original_cells = formatCells(cell_string)
  
    print('start')
-   for reihe in original_cells:
-      print(reihe)
+   for row in original_cells:
+      print(row)
       
    for number_generations in range(number_generations):
       print('Generation: ', number_generations + 1)
+      #zählt die Nummern der generationen hoch
       original_cells = gameOfLife(original_cells)
    print ('finish')   
    
 main()
+#wird aufgerufen
